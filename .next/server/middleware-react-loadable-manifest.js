ab931ba9162db556803c58de535bf01671edd725
cyril florita <cyril.florita@pm.me>
2025-04-07 18:34:30 -0700

    updated build

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"app/portfolio/page.js -> @/components/PortfolioIsotope\":{\"id\":\"app/portfolio/page.js -> @/components/PortfolioIsotope\",\"files\":[\"static/chunks/_app-pages-browser_components_PortfolioIsotope_js.js\"]}}"
