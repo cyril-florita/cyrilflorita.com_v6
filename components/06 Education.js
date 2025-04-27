251d7be333d038f1233edb64be7062bec6f26b89
cyril florita <cyril.florita@pm.me>
2025-04-26 23:20:10 -0700

    fixed educational institutions link

--- components/06 Education.js
+++ components/06 Education.js
@@ -25,7 +25,7 @@ const Education = () => {
                   <span className="cyril-accent">Bachelor</span><span className="cyril-lower"> of </span><span className="cyril-accent">Arts</span> <span className="cyril-lower">in</span> Web Design<span className="cyril-lower"> &amp; </span>Multimedia
                 </p>
                 <p className="cyril-upper cyril-text-normal cyril-mb-30">
-                  <a className="cyril-text-underline" href="https://www.linkedin.com/school/westwood-college-los-angeles">Westwood College</a> &#40;2003 &mdash; 2006&#41;
+                  <a className="cyril-text-underline" href="https://www.linkedin.com/school/westwood-college-los-angeles" target="_blank">Westwood College</a> &#40;2003 &mdash; 2006&#41;
                 </p>
                 <p>
                   This B.A. degree enhanced my technical skills in design, multimedia, and web development, preparing me to thrive in a rapidly evolving industry for diverse roles in a competitive job market.
@@ -43,7 +43,7 @@ const Education = () => {
                   <span className="cyril-accent">Master</span><span className="cyril-lower"> of </span>Divinity
                 </p>
                 <p className="cyril-upper cyril-text-normal cyril-mb-30">
-                  <a className="cyril-text-underline" href="https://www.tms.edu">The Master's Seminary</a>  &#40;2015 &mdash; 2024&#41;
+                  <a className="cyril-text-underline" href="https://www.tms.edu" target="_blank">The Master's Seminary</a>  &#40;2015 &mdash; 2024&#41;
                 </p>
                 <p>
                   Though this Master's degree was for pastoral ministry, it honed my skills in thinking critically, communicating precisely and clearly, collaborating with colleagues while resolving conflicts with integrity and professionalism, leading with courage and empathy, and managing multiple projects even under pressure and duress.
